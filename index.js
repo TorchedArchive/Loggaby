95e9d5904d8e0358c1fe9c1b64fb24d4e4ad576b
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-07-15 09:10:22 -0400

    clean up code and check if options is array

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 const ansiplace = require("ansiplace")
 class Loggaby {
-	constructor(options) {
-		this.options = options || {}
-		if (typeof this.options !== "object" && this.options !== null) throw new TypeError("'options' has to be an object.")
+	constructor(options = {}) {
+		if (Array.isArray(options) || typeof options !== "object" && options !== null) throw new TypeError("'options' has to be an object.")
+		this.options = Object.assign({debug: true}, options)
 	}
 
-	log(msg, color = !this.options.color, logfunc = this.options.logFunction ? this.options.logFunction : console.log) {
+	log(msg, color = !this.options.color, logfunc = this.options.logFunction || console.info) {
 		logfunc(color ? this._print(msg, "cyan", "Log") : `${this._time} Log > ${msg}`)
 	}
 
-	debug(msg, color = !this.options.color, logfunc = this.options.logFunction ? this.options.logFunction : console.debug) {
-		logfunc(color ? this._print(msg, "green", "Debug") : `${this._time} Debug > ${msg}`)
+	debug(msg, color = !this.options.color, logfunc = this.options.logFunction || console.debug) {
+		if(this.options.debug) return logfunc(color ? this._print(msg, "green", "Debug") : `${this._time} Debug > ${msg}`)
 	}
 
-	warn(msg, color = !this.options.color, logfunc = this.options.logFunction ? this.options.logFunction : console.warn) {
+	warn(msg, color = !this.options.color, logfunc = this.options.logFunction || console.warn) {
 		logfunc(color ? this._print(msg, "yellow", "Warn") : `${this._time} Warn > ${msg}`)
 	}
 
-	error(msg, color = !this.options.color, logfunc = this.options.logFunction ? this.options.logFunction : console.error) {
+	error(msg, color = !this.options.color, logfunc = this.options.logFunction || console.error) {
 		logfunc(color ? this._print(msg, "red", "Error") : `${this._time} Error > ${msg}`)
 	}
 
-	fatal(msg, color = !this.options.color, logfunc = this.options.logFunction ? this.options.logFunction : console.error) {
+	fatal(msg, color = !this.options.color, logfunc = this.options.logFunction || console.error) {
 		logfunc(color ? this._print(msg, "red", "Fatal", true) : `${this._time} Fatal > ${msg}`)
 	}
 
@@ -34,7 +34,7 @@ class Loggaby {
 			hour: "numeric", minute: "numeric", second: "numeric",
 			hour12: true
 		}
-		return `${new Intl.DateTimeFormat({}, options).format(new Date())}`
+		return new Intl.DateTimeFormat({}, options).format(new Date())
 	}
 }
 
